c039dd96c705290e33918cab43021e482cdf3132
Mrityunjay Samanta <samantamrityunjay98@gmail.com>
2020-12-25 23:15:05 +0530

    slider skill

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -20,3 +20,11 @@ const navSlide=() =>{
 }
 navSlide();
 
+var counter=1;
+setInterval(function(){
+    document.getElementById('radio'+counter).checked=true;
+    counter++;
+    if(counter>2){
+        counter=1;
+    }
+},5000);
