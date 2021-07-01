70c32931c7b555b8c34c07a54388a0c4fa379184
Mrityunjay Samanta <samantamrityunjay98@gmail.com>
2021-07-01 20:28:48 +0530

    Updated Content

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,12 +1,5 @@
 
-var counter=1;
-setInterval(function(){
-    document.getElementById('radio'+counter).checked=true;
-    counter++;
-    if(counter>2){
-        counter=1;
-    }
-},5000);
+
 
 const body = document.querySelector("body");
 const navbar = document.querySelector(".topnav");
